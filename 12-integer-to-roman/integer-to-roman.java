9cd077832a268d90a698c55b5bc0bb63cb6f30e3
Muhammad Talha Bilal <157731418+TBtech18@users.noreply.github.com>
2024-04-30 23:38:07 +0500

    Time: 3 ms (97.14%) | Memory: 44 MB (82.62%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V","IV", "I"};
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                result.append(symbols[i]);
+            }
+        }
+        
+        return result.toString();
+    }
+    
+    public static void main(String[] args) {
+        Solution converter = new Solution();
+        int number = 3546; 
+        String roman = converter.intToRoman(number);
+        System.out.println("Roman numeral for " + number + " is: " + roman);
+    }
+}
+
